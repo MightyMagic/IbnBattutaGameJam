1e0e7f8184524fa7b29af9472ff538b01a05b81d
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-11-15 19:58:23 +0400

    Detect clicked regions

--- Assets/Scripts/Map/GlobeController.cs
+++ Assets/Scripts/Map/GlobeController.cs
@@ -1,8 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GlobeController : MonoBehaviour
 {
+    [Header("Rotation Settings")]
     [SerializeField] private float rotationSpeed = 100f;
+
+    [Header("Regions")]
+    [SerializeField] private GameObject targetRegion;
 
     void Update()
     {
@@ -13,5 +18,19 @@ public class GlobeController : MonoBehaviour
         float rotateY = horizontal * rotationSpeed * Time.deltaTime;
 
         transform.Rotate(rotateX, rotateY, 0, Space.World);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("MapRegion"))
+                    targetRegion = hit.transform.gameObject;
+                else
+                    targetRegion = null;
+            }
+        }
     }
 }

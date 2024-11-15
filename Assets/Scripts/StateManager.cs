f82a5fa55acc588f6d62b2b5c0081858e91252bc
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-11-15 17:57:33 +0400

    Add State Manager

---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    public static StateManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}

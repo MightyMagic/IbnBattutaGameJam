1e0768d02226ef263cb4119456b82e31bf39ebb8
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-11-15 21:09:20 +0400

    Move click detection to state manager

--- Assets/Scripts/Map/GlobeController.cs
+++ Assets/Scripts/Map/GlobeController.cs
@@ -1,13 +1,9 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GlobeController : MonoBehaviour
 {
     [Header("Rotation Settings")]
     [SerializeField] private float rotationSpeed = 100f;
-
-    [Header("Regions")]
-    [SerializeField] private GameObject targetRegion;
 
     void Update()
     {
@@ -18,19 +14,5 @@ public class GlobeController : MonoBehaviour
         float rotateY = horizontal * rotationSpeed * Time.deltaTime;
 
         transform.Rotate(rotateX, rotateY, 0, Space.World);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform.CompareTag("MapRegion"))
-                    targetRegion = hit.transform.gameObject;
-                else
-                    targetRegion = null;
-            }
-        }
     }
 }
--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class StateManager : MonoBehaviour
 {
+
+    [Header("Regions")]
+    [SerializeField] private GameObject currentRegion;
+    [SerializeField] private GameObject targetRegion;
+    [SerializeField] private float travelDistance;
+
     public static StateManager Instance { get; private set; }
 
     private void Awake()
@@ -13,6 +19,30 @@ public class StateManager : MonoBehaviour
         else
         {
             Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("MapRegion"))
+                {
+                    targetRegion = hit.transform.gameObject;
+                    travelDistance = Vector3.Distance(
+                        currentRegion.transform.position,
+                        targetRegion.transform.position
+                    ) * 1000f;
+                }
+                else {
+                    targetRegion = null;
+                }
+            }
         }
     }
 }

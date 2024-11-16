2a0d8613fffa72ef1d1f2a686da35285ea357a63
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-11-16 19:53:20 +0400

    Get rid of the arrow

--- Assets/Scripts/Map/GlobeController.cs
+++ Assets/Scripts/Map/GlobeController.cs
@@ -14,10 +14,6 @@ public class GlobeController : MonoBehaviour
     [Header("Sound Effects")]
     [SerializeField] private AudioClip pickRegionSound;
 
-    [Header("Travel Trajectory")]
-    [SerializeField] private int curveResolution = 10;
-    [SerializeField] private float curveHeight = 1f;
-
     private LineRenderer lineRenderer;
 
     private void Start()
@@ -66,34 +62,5 @@ public class GlobeController : MonoBehaviour
                 }
             }
         }
-
-        if (targetRegion != null && currentRegion != targetRegion)
-        {
-            Vector3 controlPoint = (currentRegion.transform.position + targetRegion.transform.position) / 2;
-            controlPoint += Vector3.up * curveHeight;
-
-            // Vector3 midPoint = (currentRegion.transform.position + targetRegion.transform.position) / 2;
-            // Vector3 direction = (targetRegion.transform.position - currentRegion.transform.position).normalized;
-            // Vector3 curveDirection = Vector3.Cross(direction, Vector3.up).normalized;
-            // Vector3 controlPoint = midPoint + curveDirection * curveHeight;
-
-            Vector3[] points = new Vector3[curveResolution];
-            for (int i = 0; i < curveResolution; i++)
-            {
-                float t = i / (float)(curveResolution - 1);
-                points[i] = CalculateBezierPoint(t, currentRegion.transform.position, controlPoint, targetRegion.transform.position);
-            }
-
-            lineRenderer.positionCount = points.Length;
-            lineRenderer.SetPositions(points);
-        }
     }
-
-    Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
-    {
-        return (1 - t) * (1 - t) * p0 +
-               2 * (1 - t) * t * p1 +
-               t * t * p2;
-    }
-
 }

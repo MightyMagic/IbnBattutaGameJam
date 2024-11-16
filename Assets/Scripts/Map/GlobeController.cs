848df4f9647efb3809df4f4e141d113992cd943e
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-11-16 17:22:12 +0400

    Code split between State Manager and Globe

--- Assets/Scripts/Map/GlobeController.cs
+++ Assets/Scripts/Map/GlobeController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GlobeController : MonoBehaviour
@@ -5,7 +6,26 @@ public class GlobeController : MonoBehaviour
     [Header("Rotation Settings")]
     [SerializeField] private float rotationSpeed = 100f;
 
-    void Update()
+    [Header("Regions")]
+    [SerializeField] private GameObject currentRegion;
+    [SerializeField] private GameObject targetRegion;
+    [SerializeField] private float travelDistance;
+
+    [Header("Sound Effects")]
+    [SerializeField] private AudioClip pickRegionSound;
+
+    [Header("Travel Trajectory")]
+    [SerializeField] private int curveResolution = 10;
+    [SerializeField] private float curveHeight = 1f;
+
+    private LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -14,5 +34,66 @@ public class GlobeController : MonoBehaviour
         float rotateY = horizontal * rotationSpeed * Time.deltaTime;
 
         transform.Rotate(rotateX, rotateY, 0, Space.World);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("MapRegion"))
+                {
+                    targetRegion = hit.transform.gameObject;
+
+                    travelDistance = Vector3.Distance(
+                        currentRegion.transform.position,
+                        targetRegion.transform.position
+                    ) * 1000f;
+
+                    StateManager.Instance.SetTravel(
+                        currentRegion.GetComponent<MapRegion>().regionName,
+                        targetRegion.GetComponent<MapRegion>().regionName,
+                        travelDistance,
+                        targetRegion.GetComponent<MapRegion>().resourceType
+                    );
+
+                    AudioSource.PlayClipAtPoint(pickRegionSound, Camera.main.transform.position);
+                }
+                else {
+                    targetRegion = null;
+                    lineRenderer.SetPositions(new Vector3[0]);
+                }
+            }
+        }
+
+        if (targetRegion != null && currentRegion != targetRegion)
+        {
+            Vector3 controlPoint = (currentRegion.transform.position + targetRegion.transform.position) / 2;
+            controlPoint += Vector3.up * curveHeight;
+
+            // Vector3 midPoint = (currentRegion.transform.position + targetRegion.transform.position) / 2;
+            // Vector3 direction = (targetRegion.transform.position - currentRegion.transform.position).normalized;
+            // Vector3 curveDirection = Vector3.Cross(direction, Vector3.up).normalized;
+            // Vector3 controlPoint = midPoint + curveDirection * curveHeight;
+
+            Vector3[] points = new Vector3[curveResolution];
+            for (int i = 0; i < curveResolution; i++)
+            {
+                float t = i / (float)(curveResolution - 1);
+                points[i] = CalculateBezierPoint(t, currentRegion.transform.position, controlPoint, targetRegion.transform.position);
+            }
+
+            lineRenderer.positionCount = points.Length;
+            lineRenderer.SetPositions(points);
+        }
     }
+
+    Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        return (1 - t) * (1 - t) * p0 +
+               2 * (1 - t) * t * p1 +
+               t * t * p2;
+    }
+
 }
--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -4,31 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class StateManager : MonoBehaviour
 {
-
-    [Header("Regions")]
-    [SerializeField] private GameObject currentRegion;
-    [SerializeField] private GameObject targetRegion;
-    [SerializeField] private float travelDistance;
-
-    [Header("Travel Trajectory")]
-    [SerializeField] private int curveResolution = 10;
-    [SerializeField] private float curveHeight = 1f;
-    private LineRenderer lineRenderer;
     public static StateManager Instance { get; private set; }
 
     [Header("Location References")]
-    [SerializeField] private string from;
-    [SerializeField] private string to;
-    [SerializeField] private string trading;
+    [SerializeField] private string _from;
+    [SerializeField] private string _to;
+    [SerializeField] private string _trading;
+    [SerializeField] private float _travelDistance;
 
     [Header("Resources")]
     [SerializeField] private int metals = 0;
     [SerializeField] private int grain = 0;
     [SerializeField] private int spices = 10;
     [SerializeField] private int silk = 0;
-
-    [Header("Sound Effects")]
-    [SerializeField] private AudioClip pickRegionSound;
 
     private void Awake()
     {
@@ -43,72 +31,17 @@ public class StateManager : MonoBehaviour
         }
     }
 
-    private void Start()
+    public void SetTravel(string from, string to, float travelDistance, string trading)
     {
-        lineRenderer = GetComponent<LineRenderer>();
-    }
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform.CompareTag("MapRegion"))
-                {
-                    targetRegion = hit.transform.gameObject;
-
-                    from = currentRegion.GetComponent<MapRegion>().regionName;
-                    to = targetRegion.GetComponent<MapRegion>().regionName;
-                    trading = targetRegion.GetComponent<MapRegion>().resourceType;
-
-                    travelDistance = Vector3.Distance(
-                        currentRegion.transform.position,
-                        targetRegion.transform.position
-                    ) * 1000f;
-
-                    AudioSource.PlayClipAtPoint(pickRegionSound, Camera.main.transform.position);
-                }
-                else {
-                    targetRegion = null;
-                }
-            }
-        }
-
-        if (targetRegion != null && currentRegion != targetRegion)
-        {
-            Vector3 controlPoint = (currentRegion.transform.position + targetRegion.transform.position) / 2;
-            controlPoint += Vector3.up * curveHeight;
-
-            // Vector3 midPoint = (currentRegion.transform.position + targetRegion.transform.position) / 2;
-            // Vector3 direction = (targetRegion.transform.position - currentRegion.transform.position).normalized; // Direction of the curve
-            // Vector3 curveDirection = Vector3.Cross(direction, Vector3.up).normalized; // Perpendicular vector for curve
-            // Vector3 controlPoint = midPoint + curveDirection * curveHeight;
-
-            Vector3[] points = new Vector3[curveResolution];
-            for (int i = 0; i < curveResolution; i++)
-            {
-                float t = i / (float)(curveResolution - 1);
-                points[i] = CalculateBezierPoint(t, currentRegion.transform.position, controlPoint, targetRegion.transform.position);
-            }
-
-            lineRenderer.positionCount = points.Length;
-            lineRenderer.SetPositions(points);
-        }
-    }
-
-    Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
-    {
-        return (1 - t) * (1 - t) * p0 +
-               2 * (1 - t) * t * p1 +
-               t * t * p2;
+        _from = from;
+        _to = to;
+        _trading = trading;
+        _travelDistance = travelDistance;
     }
 
     public void StartTravel()
     {
-        if (travelDistance > 0)
+        if (_travelDistance > 0)
         {
             SceneManager.LoadScene("DynamicLevelPrototype");
         }

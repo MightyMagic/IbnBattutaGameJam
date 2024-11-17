81e3f0c49aebd6938b703463d8977e82ac4d4808
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-11-17 09:19:47 +0400

    Make region canvas a prefab

--- Assets/Scripts/Map/MapRegion.cs
+++ Assets/Scripts/Map/MapRegion.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapRegion : MonoBehaviour
 {
@@ -10,12 +11,20 @@ public class MapRegion : MonoBehaviour
     [SerializeField] private string midTrade;
     [SerializeField] private string minTrade;
 
+    [Header("Region UI")]
+    [SerializeField] private Image resourceImage;
+
     [SerializeField] private float scaleMultiplier = 1.2f;
     private Vector3 originalScale;
 
     private void Start()
     {
         originalScale = transform.localScale;
+
+        if (resourceImage != null)
+        {
+            resourceImage.sprite = Resources.Load<Sprite>($"Icons/{resourceType}");
+        }
     }
 
     private void OnMouseOver()
